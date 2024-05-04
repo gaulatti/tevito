9c9c261e25c03b267307c4354e535c6210c0264f
Javier Godoy <rotvulpix@gmail.com>
2024-05-04 15:10:28 -0400

    Adding Antofagasta TV

--- tevito/views/PlayerView.swift
+++ tevito/views/PlayerView.swift
@@ -11,7 +11,8 @@ struct PlayerView: View {
     let videos = [
         Video(name: "BBCL", url: URL(string: "https://redirector.rudo.video/hls-video/339f69c6122f6d8f4574732c235f09b7683e31a5/bbtv/bbtv.smil/playlist.m3u8")!),
         Video(name: "Schlager Deluxe", url: URL(string: "https://sdn-global-live-streaming-packager-cache.3qsdn.com/26658/26658_264_live.m3u8")!),
-        Video(name: "RTL 102.5", url: URL(string: "https://streamcdnc1-dd782ed59e2a4e86aabf6fc508674b59.msvdn.net/live/S97044836/tbbP8T1ZRPBL/playlist_video.m3u8")!)
+        Video(name: "RTL 102.5", url: URL(string: "https://streamcdnc1-dd782ed59e2a4e86aabf6fc508674b59.msvdn.net/live/S97044836/tbbP8T1ZRPBL/playlist_video.m3u8")!),
+        Video(name: "Antofagasta TV", url: URL(string: "https://unlimited1-us.dps.live/atv/atv.smil/playlist.m3u8")!)
     ]
     
     var body: some View {
@@ -47,3 +48,4 @@ struct ContentView_Previews: PreviewProvider {
         PlayerView()
     }
 }
+ 
