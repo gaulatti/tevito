018774047c118a1f771ac970a4f3c008436d7eb0
Javier Godoy <rotvulpix@gmail.com>
2024-05-15 18:51:59 -0400

    feat: Adding radioitalia. closes #6

--- tevito/views/PlayerView.swift
+++ tevito/views/PlayerView.swift
@@ -12,7 +12,8 @@ struct PlayerView: View {
         Video(name: "BBCL", url: URL(string: "https://redirector.rudo.video/hls-video/339f69c6122f6d8f4574732c235f09b7683e31a5/bbtv/bbtv.smil/playlist.m3u8")!),
         Video(name: "Schlager Deluxe", url: URL(string: "https://sdn-global-live-streaming-packager-cache.3qsdn.com/26658/26658_264_live.m3u8")!),
         Video(name: "RTL 102.5", url: URL(string: "https://streamcdnc1-dd782ed59e2a4e86aabf6fc508674b59.msvdn.net/live/S97044836/tbbP8T1ZRPBL/playlist_video.m3u8")!),
-        Video(name: "Antofagasta TV", url: URL(string: "https://unlimited1-us.dps.live/atv/atv.smil/playlist.m3u8")!)
+        Video(name: "Antofagasta TV", url: URL(string: "https://unlimited1-us.dps.live/atv/atv.smil/playlist.m3u8")!),
+        Video(name: "RadioItalia", url: URL(string: "https://radioitaliatv.akamaized.net/hls/live/2093117/RadioitaliaTV/master.m3u8")!)
     ]
     
     var body: some View {
